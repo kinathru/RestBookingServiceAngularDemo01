ad0ba31a1c43cbedc2e2989711794b0e7e62f51f
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-03-01 23:28:59 +0530

    initial commit of angular demo

---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {UserComponent} from './components/user.component';
+import {AboutComponent} from './components/about.component';
+import { BookingSearchComponent } from './components/bookingsearch.component';
+
+const appRoutes : Routes = [
+    {
+        path:'',
+        component: UserComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'bookingsearch',
+        component:BookingSearchComponent
+    }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
